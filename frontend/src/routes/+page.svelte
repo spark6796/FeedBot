32a165cb6e5e035f4621259d246bfe0f50527e83
spark6796 <daksh2020d@gmail.com>
2025-11-06 23:59:01 +0530

    Feat: Add homepage

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="bg h-screen w-full">
+ 
+    <div class="h-1/4 w-full pt-8">
+        <img src="/title.png" alt="Feed bot" class="mx-auto">
+    </div>
+    <div class="flex justify-center items-center h-3/4 w-full">
+        <div class="flex transition-transform hover:scale-105 hover:border-sky-700 bungee-regular text-4xl font-bold bg-slate-800 border-[#154467] justify-center items-center h-fit w-fit p-8 text-center rounded-xl text-white border-8">
+            Continue with discord
+            <img src="/discord.svg" alt="discord logo" class="pl-4">
+        </div>
+    </div>
+</div>
